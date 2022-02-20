cad980443ef331081c31f4d453132a740da39157
Diganto Bhowmik <digantobhowmik@gmail.com>
2022-02-21 03:07:13 +0600

    Product list

---
+++ Products/Products/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Products.Models.Database;
+
+namespace Products.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        [HttpGet]
+        public ActionResult ProductsList()
+        {
+            Entities db = new Entities();
+            var data = db.Products.ToList();
+            return View(data);
+        }
+    }
+}
